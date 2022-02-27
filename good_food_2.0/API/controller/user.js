eee8f89eb4314df223476733c6fe1fc29d91fe2d
Olivier BACONNAIS <olivier.baconnais@icloud.com>
2022-02-27 09:43:46 +0100

    writing test about user create db connection

--- good_food_2.0/API/controller/user.js
+++ good_food_2.0/API/controller/user.js
@@ -2,23 +2,36 @@ const User = require("../model/user")
 
 module.exports.createUser = async (user) => {
     try{
+        //testing if needed informations about user are not null
+        if(user.lastname == null || user.forname == null || user.mail == null){
+            console.log(`user must have lastname, forname and email`)
+            return -1
+        }
+
         const existingUser = await User.findOne(user)
         //testing if user not exist
-        if(existingUser)
-            throw new Error(`user ${lastname} already exist`)
-
+        if(existingUser){
+            console.log(`user ${user.lastname} ${user.forname} already exist`)
+            return -1
+        }
+            
         //create user in the db
-        await User.create(user).then(console.log(`the user ${user.lastname} ${user.forname} created successfully`))
+        await User.create(user).then(()=>{
+            console.log(`the user ${user.lastname} ${user.forname} created successfully`)
+            return 1
+        })
+        
     }catch(err){throw err}
 }
 
 module.exports.findUser = async (user)=>{
     try{
-
         const existingUser = await User.findOne(user)
         //testing if user not exist
-        if(!existingUser)
-            return (null,console.log(`user ${user.lastname} ${user.forname} doesn't exist`))
+        if(!existingUser){
+            console.log(`user ${user.lastname} ${user.forname} doesn't exist`)
+            return null
+        }  
         else{
             console.log(`user ${user.lastname} ${user.forname} exist`)
             return user
--- good_food_2.0/API/tests/route.test.js
+++ good_food_2.0/API/tests/route.test.js
@@ -14,9 +14,53 @@ afterAll(async ()=> await db.closeDatabase())
     let address = "2 rue saint Simon 64000 PAU"
     const UserComplet = new user({lastname,forname,mail,address})
 
-    it('first user', async () => {
+    it('create user normally', async () => {
         await User.createUser(UserComplet)        
         let existeUser = await User.findUser(UserComplet)
         expect(existeUser).toBe(UserComplet)
     }) 
+
+    it('create user without lastname', async () => {
+        UserUncomplet = UserComplet
+        UserUncomplet.lastname = null
+        await User.createUser(UserComplet)        
+        let existeUser = await User.findUser(UserComplet)
+        expect(existeUser).toBe(null)
+    }) 
+
+     it('create user without forname', async () => {
+        UserUncomplet = UserComplet
+        UserUncomplet.forname = null
+        await User.createUser(UserComplet)        
+        let existeUser = await User.findUser(UserComplet)
+        expect(existeUser).toBe(null)
+    }) 
+
+    it('create user without mail', async () => {
+        UserUncomplet = UserComplet
+        UserUncomplet.mail = null
+        await User.createUser(UserComplet)        
+        let existeUser = await User.findUser(UserComplet)
+        expect(existeUser).toBe(null)
+    }) 
+
+    it('create user without needed fields', async () => {
+        UserUncomplet = UserComplet
+        UserUncomplet.mail = null
+        UserUncomplet.forname = null
+        UserUncomplet.lastna = null
+        await User.createUser(UserComplet)        
+        let existeUser = await User.findUser(UserComplet)
+        expect(existeUser).toBe(null)
+    }) 
+
+    it('create user who are already created', async () =>{
+        await User.createUser(UserComplet)
+        let alreadyCreatedUser = await User.createUser(UserComplet)        
+        expect(alreadyCreatedUser).toBe(-1)
+    } )
+    
+    it('find user', async () => {
+        console.log("test")
+    })
 })
