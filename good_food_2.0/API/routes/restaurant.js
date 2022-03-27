c2dce1b9aad704ce564ece9ce5d32f5e71614f75
floriancohen40 <55598927+floriancohen40@users.noreply.github.com>
2022-03-27 20:09:32 +0200

    feat: routes
    Added routes :
    - Update restaurant
    - Delete restaurant
    - Find restaurant by ID
    - Find all restaurants
    
    Renamed routes :
    - Create restaurant
    - Find restaurant by name

--- good_food_2.0/API/routes/restaurant.js
+++ good_food_2.0/API/routes/restaurant.js
@@ -10,6 +10,17 @@ module.exports = function (router) {
     /***********************************************/
     /*********** routage de la ressource ***********/
     /***********************************************/
-    router.get('/find/:name',restaurantCtrl.findRestaurantByName)
-    router.put('/restaurant/create',restaurantCtrl.createRestaurant)
+    // Create restaurant
+    router.put('/restaurant',restaurantCtrl.createRestaurant)
+
+    // Update restaurant
+    router.patch('/restaurant/:id', restaurantCtrl.updateRestaurant)
+
+    // Delete restaurant
+    router.delete('/restaurant/:id', restaurantCtrl.removeRestaurant)
+
+    // Find restaurants
+    router.get('/restaurant/:name',restaurantCtrl.findRestaurantByName)
+    router.get('/restaurant/id/:id',restaurantCtrl.findRestaurantById)
+    router.get('/restaurants', restaurantCtrl.findAllRestaurants)
 }
