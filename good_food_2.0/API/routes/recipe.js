52a2b0de7445f03d361da3da12bed71c67c9471b
JulienChampagnol <julien.champagnol@geode-solutions.com>
2022-04-06 22:05:58 +0200

    recipe controler

--- good_food_2.0/API/routes/recipe.js
+++ good_food_2.0/API/routes/recipe.js
@@ -1,18 +1,49 @@
 /***********************************************/
 /********* import necessary librairies *********/
 /***********************************************/
-const recipeCtrl = require('./controler/recipe')
-const express = require('express')
+
+const express = require("express")
+const recipeCtrl = require("../controler/recipe")
 
 /***********************************************/
 /******** definition of request on API *********/
 /***********************************************/
 let router = express.Router()
 
+router.use((req, res, next) => {
+    const event = new Date()
+    console.log(`User time : ${event.toString()}`)
+    next()
+})
+/***********************************************/
+/*************** route resources ***************/
+/***********************************************/
 
-/***********************************************/
-/*********** routage de la ressource ***********/
-/***********************************************/
-router.put('', recipeCtrl.createRecipe)
+router.get('', recipeCtrl.getAllRecipes)
+
+/**
+* route to create a user
+*/
+// router.put('', recipeCtrl.createRecipe)
+
+/**
+* route to get the user_Id thanks to its mail 
+*/
+// router.get('/mail', recipeCtrl.getRecipeId)
+
+/**
+* route to get a user thanks to its id
+*/
+// router.get('/:id', recipeCtrl.getRecipe)
+
+/**
+* route to delete a user
+*/
+// router.delete('/:id', recipeCtrl.deleteRecipe)
+
+/**
+* route to set a user
+*/
+// router.patch('/:id', recipeCtrl.setRecipe)
 
 module.exports = router
