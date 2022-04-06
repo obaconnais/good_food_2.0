6d4dff73f5475f43fd83aff8f5e1395fff0ee433
JulienChampagnol <julien.champagnol@geode-solutions.com>
2022-04-06 22:08:08 +0200

    server

--- good_food_2.0/API/serveur.js
+++ good_food_2.0/API/serveur.js
@@ -1,24 +1,22 @@
 /*******************************************/
-/************** library import *************/
-/*******************************************/
-const express = require('express')
-const cors = require('cors')
-
-
-/*******************************************/
 /**************** API init *****************/
 /*******************************************/
+let express = require('express')
+let cors = require('cors')
 const app = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get('/', (req, res) => res.status(501).send(`What the fuck ?`))
 // const user_router = require('./routes/user')
-const recipe_router = require('./routes/recipe')
-
+// app.get('/', (req, res) =>"")
+app.get('/', (req, res) => res.status(501).send(`What the fuck ?`))
 // app.get('user', (req, res) => user_router)
-app.get('recipe', (req, res) => recipe_router)
+
+const recipe_router = require('./routes/recipe')
+// app.get('/', (req, res) =>"")
+// recipe_router.get('/', (req, res) => res.status(501).send(`What the fuck ?`))
+// app.get('user', (req, res) => user_router)
 
 const port = 5000
 const server = app.listen(port, () => console.log(`server is listening on port ${port}`))
