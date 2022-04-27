83082fdbf139cb4794c9b2890d55055e5d0c4bb0
Indie Blanc <indie.blanc@viacesi.fr>
2022-04-27 18:13:07 +0200

    Update restaurant model
    Set mail to unique date

--- good_food_2.0/API/model/restaurant.js
+++ good_food_2.0/API/model/restaurant.js
@@ -19,6 +19,7 @@ const restaurantSchema = new Schema(
         },
         mail: {
             type: String,
+            unique: true,
             required: [true, 'Required field'],
             match: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/                
         },
