4c0686f78a2e372acfcd2b0c8c794f1070fda748
Olivier BACONNAIS <olivier.baconnais@icloud.com>
2022-05-12 22:42:20 +0200

    correct some mistake

--- frontClient/src/app/home/home-franchise/home-franchise.component.ts
+++ frontClient/src/app/home/home-franchise/home-franchise.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from "leaflet"
-import { IRestaurants } from 'src/app/_interface/restaurant';
 import { RestaurantService } from 'src/app/_service/restaurant.service';
 import { GeoCorderResponseService } from 'src/app/_service/geo-corder-response.service';
-import { zip } from 'rxjs';
 
 @Component({
   selector: 'app-home-franchise',
   templateUrl: './home-franchise.component.html',
   styleUrls: ['./home-franchise.component.css']
 })
+
 export class HomeFranchiseComponent implements OnInit {
   constructor(
     private restaurants:RestaurantService,
@@ -21,17 +20,18 @@ export class HomeFranchiseComponent implements OnInit {
     //get addresse from database and convert it into coordinate
     this.restaurants.getRestaurant().subscribe(rest=>{
       rest.data.forEach(restaurant=>{
+        //translate string to geo coordinate
         this.geoService.getLocation(restaurant.address).subscribe(coord=>{
-          console.log()
           const myIcon = L.icon({
             iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.2.0/images/marker-icon.png'
           });
+          //add it to the map
           L.marker([coord.data[0].latitude, coord.data[0].longitude], {icon: myIcon}).bindPopup(`${restaurant.name}`).addTo(myMap).openPopup();
         })
       })
     })
 
-
+    //usefull for displaying map and center it
     const myMap = L.map('map').setView([46.990896, 3.162845], 5)
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
       attribution: 'myMap'
