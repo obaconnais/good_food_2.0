29eb3794bca7ed085e9a9c21823e3092de9df1d6
Olivier BACONNAIS <olivier.baconnais@icloud.com>
2022-05-31 16:16:11 +0200

    ajout test

---
+++ frontClient/src/app/_interface/recipe.ts
@@ -0,0 +1,10 @@
+export interface IRecipe {
+  name: string,
+  ingredients:[string],
+  price:number,
+  restaurant_id:[string]
+}
+
+export interface IRecipes{
+  recipes:[IRecipe]
+}
---
+++ frontClient/src/app/_service/recipe.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IRecipes } from '../_interface/recipe';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipeService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getRecipes():Observable<IRecipes>{
+    return this.httpClient.get<IRecipes>('http://localhost:5001/recipe')
+  }
+}
